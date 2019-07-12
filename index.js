3472441f016bfbf5d011124cb3c8ea5335fbf068
effective-role-2041 <steve.alex@hotmail.co.uk>
2019-07-12 15:14:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return getFirstSelector("#nested").querySelector(".target")
+}
+
+function increaseRankBy(n){
+  var rank = document.getElementById("app").querySelectorAll("ul.ranked-list")
+  for (var i=0; i<rank.length; i++){
+    rank[i].innerHTML = parseInt((rank[i].innerHTML + n));
+  }
+}
+
+function deepestChild(){
+  
+}
