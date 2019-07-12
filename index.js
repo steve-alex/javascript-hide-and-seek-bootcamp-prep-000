8179c113f28ef0f36b8e3edd43e1e5f0707159c3
effective-role-2041 <steve.alex@hotmail.co.uk>
2019-07-12 15:19:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return getFirstSelector("#nested").querySelector(".target")
+}
+
+function increaseRankBy(n){
+  var rank = document.getElementById("app").querySelectorAll("ul.ranked-list")
+  for (var i=0; i<rank.length; i++){
+    rank[i].innerHTML = parseInt((rank[i].innerHTML + n));
+  }
+}
+
+function deepestChild(){
+  var head = document.getElementById('grand-node').querySelectorAll('div');
+  return head[head.length - 1]
+}
